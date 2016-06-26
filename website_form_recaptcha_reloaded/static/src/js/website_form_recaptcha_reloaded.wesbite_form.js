3d3cbb9f16c702fcd44c99e997cb684b821721f8
Abdelrahman Ghanem <abom.jdev@gmail.com>
2016-06-26 09:09:04 +0200

    Show a message that guides the user to solve captcha first...
    This message indicates that the captcha is required, also fields validation would occure first.
    Modifying recaptcha_defined(), so recaptcha is defined only if there is an element with g-recaptcha class.

--- website_form_recaptcha_reloaded/static/src/js/website_form_recaptcha_reloaded.wesbite_form.js
+++ website_form_recaptcha_reloaded/static/src/js/website_form_recaptcha_reloaded.wesbite_form.js
@@ -1,28 +1,44 @@
 odoo.define('website_form_recaptcha_reloaded.website_form', function (require) {
     "use strict";
 
+    var core = require('web.core');
+    var qweb = core.qweb;
+
     var website_form = require('website_form.animation');
     var snippet_animation = require('web_editor.snippets.animation');
 
     snippet_animation.registry.form_builder_send.include({
+        start: function() {
+            qweb.add_template('/website_form_recaptcha_reloaded/static/src/xml/website_form.xml');
+            this._super();
+        },
 
         recaptcha_defined: function() {
-            return typeof grecaptcha != 'undefined';
+            return typeof grecaptcha != 'undefined' &&
+                   $('.g-recaptcha').length > 0;
         },
 
         recaptcha_valid: function() {
             return grecaptcha.getResponse() != '';
         },
 
-        check_error_fields: function(error_fields) {
+        send: function (e) {
+            // check for error fields first
+            if (!this.check_error_fields([])) {
+                this.update_status('invalid');
+                return false;
+            }
+
+            // then check for recaptcha...
             if(this.recaptcha_defined()) {
                 if(!this.recaptcha_valid()) {
+                    this.update_status('invalid_recaptcha');
                     return false;
                 }
             }
 
-            return this._super(error_fields);
-        }
+            return this._super(e);
+        },
 
     });
 
